83c6158c3acf7374b8f1b31492b95b09828e35ae
sumankumar satyadev kanti <sumankantiit@gmail.com>
2025-08-12 11:43:17 +0530

    Add files via upload

---
+++ Personalized Greeting.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting Problem Statement:
+
+f_n =input("ENTER YOUR FIRST NAME:")
+l_n =input("ENTER YOUR LAST NAME:")
+
+f = f_n + " " + l_n
+
+print(f" HEllO,{f}! WELCOME TO THE PYTHON PROGRAM." )
---
+++ calculators.py
@@ -0,0 +1,16 @@
+# Task1:Perform Basic Mathematical Operations Problem Statement
+
+
+num1=float(input("ENTER FIRST NO: "))
+num2=float(input("ENTER SCCOND NO: "))
+
+ADD = num1 + num2
+SUB = num1 - num2
+MUL = num1 * num2
+divi = num1 // num2
+
+print("ADDITION: ", ADD)
+print("SUBTRACTION:", SUB)
+print("MULTIPLICATION:", MUL)
+print("DIVISION:", divi)
+
